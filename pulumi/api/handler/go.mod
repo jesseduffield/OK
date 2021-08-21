50783f83100d8b0e75880833194aa01e5a372ffb
Jesse Duffield <jessedduffield@gmail.com>
2021-08-21 12:57:26 +1000

    more

--- pulumi/api/handler/go.mod
+++ pulumi/api/handler/go.mod
@@ -4,8 +4,5 @@ go 1.15
 
 require (
 	github.com/aws/aws-lambda-go v1.26.0
-	github.com/aws/aws-sdk-go v1.40.25 // indirect
-	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/jesseduffield/OK/ok v0.0.0-20210821025011-1f0e002c175a
-	github.com/urfave/cli/v2 v2.3.0 // indirect
 )
--- pulumi/deploy-api.sh
+++ pulumi/deploy-api.sh
@@ -6,6 +6,7 @@ cd api/handler
 # direct so that we're not waiting for the proxy to receive our latest changes
 GOPROXY=direct go get github.com/jesseduffield/OK/ok
 go get github.com/aws/aws-lambda-go/cmd/build-lambda-zip
+go mod tidy
 GOOS=linux GOARCH=amd64 go build -o handler main.go
 build-lambda-zip -output handler.zip handler
 cd ..
